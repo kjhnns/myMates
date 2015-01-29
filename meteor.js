b5c6c66a81ac1441c2ccdf6df6013502c42b28f5
Johannes Klumpe <klumpe@live.de>
2015-01-29 15:32:17 +0100

    Learning meteor

--- meteor.js
+++ meteor.js
@@ -1,23 +1,35 @@
+LearnObjects = new Mongo.Collection("learnobjects");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+    Template.body.helpers({
+        learnObjects: LearnObjects.find({}, {sort: {createdAt: -1}})
+    });
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+    Template.body.events({
+        "submit .new-task": function(event) {
+            // This function is called when the new task form is submitted
+            var text = event.target.text.value;
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+            LearnObjects.insert({
+                text: text,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.text.value = "";
+
+            // Prevent default form submit
+            return false;
+        },
+        "click .delete": function(event) {
+            // This function is called when the new task form is submitted
+            LearnObjects.remove({_id: event.target.dataset.id});
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
