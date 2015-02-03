5b3aebac710f5534bffdeafe6a419ca6e5f89c56
Johannes Klumpe <klumpe@live.de>
2015-02-03 23:47:25 +0100

    Accounts & Quotations implemented

---
+++ client/controller/main.js
@@ -0,0 +1,9 @@
+Template.main.helpers({
+  // We use #each on an array of one item so that the "list" template is
+  // removed and a new copy is added when changing lists, which is
+  // important for animation purposes. #each looks at the _id property of it's
+  // items to know when to insert a new item and when to update an old one.
+  thisArray: function() {
+    return [this];
+  }
+});
